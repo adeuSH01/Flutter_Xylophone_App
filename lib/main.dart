db41683d941b0ee12f45ac8090234cd283493359
adeuSH1 <112491144+adeuSH1@users.noreply.github.com>
2024-08-02 13:46:38 +0300

    .

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  Expanded buildKey(Color color, int soundNumber) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          final player = AudioPlayer(playerId: 'my_unique_playerId');
+          player.play(AssetSource('assets/note$soundNumber.wav'));
+        },
+        style: TextButton.styleFrom(padding: EdgeInsets.zero),
+        child: Container(
+          color: color,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.teal, 5),
+              buildKey(Colors.blue, 6),
+              buildKey(Colors.purple, 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
